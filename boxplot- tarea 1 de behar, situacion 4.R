db1a457803a805c25c1887c2d06f9d4e76541f3c
DanielaTorresLabio <151484210+DanielaTorresLabio@users.noreply.github.com>
2024-01-16 15:08:53 -0500

    Update boxplot- tarea 1 de behar, situacion 4.R

--- boxplot- tarea 1 de behar, situacion 4.R
+++ boxplot- tarea 1 de behar, situacion 4.R
@@ -4,7 +4,6 @@ library(ggplot2)
 
 #definir data.frame
 datos<-data.frame(read_xlsx("~/boxplot-frecuencia.xlsx"))
-
-
+#ejecucion del diagrama de caja 
 ggplot(data = datos,aes(datos$frecuencia.relativa......fi.,datos$frecuncia.absoluta...ni.))+geom_boxplot()+   geom_jitter(alpha = 0.5, color = "tomato")    
 
