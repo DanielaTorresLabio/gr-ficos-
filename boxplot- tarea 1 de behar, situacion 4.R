f6250f2a41047265a1ef1dce58b3f2cef59b0537
DanielaTorresLabio <151484210+DanielaTorresLabio@users.noreply.github.com>
2024-01-16 15:16:30 -0500

    Update boxplot- tarea 1 de behar, situacion 4.R

--- boxplot- tarea 1 de behar, situacion 4.R
+++ boxplot- tarea 1 de behar, situacion 4.R
@@ -4,6 +4,10 @@ library(ggplot2)
 
 #definir data.frame
 datos<-data.frame(read_xlsx("~/boxplot-frecuencia.xlsx"))
-#ejecucion del diagrama de caja 
-ggplot(data = datos,aes(datos$frecuencia.relativa......fi.,datos$frecuncia.absoluta...ni.))+geom_boxplot()+   geom_jitter(alpha = 0.5, color = "tomato")    
+
+#definir nombre de las varioables X y Y 
+datos<-data.frame(fi=datos$frecuencia.relativa......fi.,Fi=datos$frecuncia.absoluta...ni.)
+
+#ejecutar el diagrama de puntos 
+ggplot(data = datos,aes(fi,Fi))+geom_boxplot()+geom_jitter(alpha = 0.5, color = "tomato")  
 
